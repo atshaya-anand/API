1a01826121c3e462ea6022a6f39fbadfcac6b7f8
Atshaya <atshaya1699@gmail.com>
2021-01-11 21:14:50 +0530

    added set theory in py

--- Python/app.py
+++ Python/app.py
@@ -2,12 +2,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	data1 = {2,3,4,5,6,7,8}
-	data2 = {21,42,63,84}
-	result = data1.union(data2)
-	return str(result)
+# function for union - contains all elements of both set
+@app.route('/union',methods = ['Get'])
+def union(seta , setb):
+    result = set()
+    for i in seta:
+        result.add(i)
+    for i in setb:
+        result.add(i)
+    return result
+
+
+# function for intersection - contains only common elements of set
+@app.route('/intersection',methods = ['GET'])
+def intersection(seta , setb):
+    result = set()
+    for i in seta:
+        if i in setb:
+          result.add(i)
+    return result
+
+
+# function for minus - contains elements which are not in another set
+@app.route('/minus',methods = ['GET'])
+def minus(seta , setb):
+    result = set()
+    for i in seta:
+        if i not in setb:
+            result.add(i)
+    return result
+
 
 
 if __name__ == '__main__':
