61ec7431ab470fddc4d699eddf1156fcc0a01b0c
Atshaya <atshaya1699@gmail.com>
2021-02-16 19:46:12 +0530

    added php code for all programs

---
+++ UI/index.php
@@ -0,0 +1,341 @@
+<!-- Date Difference -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $start_date = $_GET['s_date'];
+    $end_date = $_GET['e_date'];
+    $start_date = date_create($start_date);
+    $end_date = date_create($end_date);
+    $diff= date_diff($end_date, $start_date);
+    $diff = $diff->format('%y years %m months %d days %h hours %i seconds');
+    $result_json = array('difference' => $diff);
+
+    header('Cache-Control: no-cache, must-revalidate');
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+    header('Content-type: application/json');
+    if (empty($start_date) || empty($end_date)) {
+        echo "Dates are incorrect syntax";
+    } else {
+        echo json_encode($result_json);
+    }
+}
+?>
+
+<!-- SET Union -->
+<?php
+   function unionFunc($array1, $array2) {
+       $result = array_merge($array1, $array2);
+       return $result;
+   }
+?>
+
+<!-- SET Intersection -->
+<?php
+   function intersectionFunc($array1, $array2) {
+       $result = array_intersect($array1, $array2);
+       return $result;
+   }
+?>
+
+<!-- SET Difference -->
+<?php
+   function diffFunc($array1, $array2) {
+       $result = array_diff($array1, $array2);
+       return $result;
+   }
+?>
+
+<!-- MATRIX - Transpose -->
+<?php
+  function transpose($str) {
+     $array = explode('|', $str);
+     $final_array = array();
+     foreach($array as $val)
+     {
+        array_push($final_array, explode(',', $val));
+     }
+     $newFoo = [];
+     foreach($final_array as $a => $k){
+        foreach($k as $i => $j){
+           $newFoo[$i][]= $j;
+        }
+     }
+     $tmpArr = array();
+     foreach ($newFoo as $sub) {
+     $tmpArr[] = implode(',', $sub);
+     }
+     $result = implode('|', $tmpArr);
+     return ($result);
+  }
+?>
+
+<!-- MATRIX - Lower Diagonal -->
+<?php
+   function lowerDiagonal($str) {
+       $array = explode('|', $str);
+       $final_array = array();
+       foreach($array as $val)
+       {
+           array_push($final_array, explode(',', $val));
+       }
+       $i; $j;
+       $row = count($final_array);
+       $col = count($final_array[0]);
+       $newArr = array();
+       for ($i = 0; $i < $row; $i++)
+       {
+           $temp_array = array();
+           for ($j = 0; $j < $col; $j++)
+           {
+               if ($i < $j)
+               {
+                   array_push($temp_array, 0);
+               }
+               else{
+                   array_push($temp_array, $final_array[$i][$j]);
+               }
+           }
+           array_push($newArr, $temp_array);
+       }
+       $tmpArr = array();
+       foreach ($newArr as $sub) {
+           $tmpArr[] = implode(',', $sub);
+       }
+       $result = implode('|', $tmpArr);
+       return ($result);
+   }
+?>
+
+<!-- MATRIX - Upper Diagonal
+<?php
+   function upperDiagonal($str) {
+       $array = explode('|', $str);
+       $final_array = array();
+       foreach($array as $val)
+       {
+           array_push($final_array, explode(',', $val));
+       }
+       $i; $j;
+       $row = count($final_array);
+       $col = count($final_array[0]);
+       $newArr = array();
+       for ($i = 0; $i < $row; $i++)
+       {
+           $temp_array = array();
+           for ($j = 0; $j < $col; $j++)
+           {
+               if ($i > $j)
+               {
+                   array_push($temp_array, 0);
+               }
+               else{
+                   array_push($temp_array, $final_array[$i][$j]);
+               }
+           }
+           array_push($newArr, $temp_array);
+       }
+       $tmpArr = array();
+       foreach ($newArr as $sub) {
+           $tmpArr[] = implode(',', $sub);
+       }
+       $result = implode('|', $tmpArr);
+       return ($result);
+   }
+?>
+
+// Figure to Word
+<?php
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+       $number =  $_POST['currency'];
+       $no = floor($number);
+       $point = round($number - $no, 2) * 100;
+       $hundred = null;
+       $digits_1 = strlen($no);
+       $i = 0;
+       $str = array();
+       $words = array('0' => '', '1' => 'one', '2' => 'two',
+       '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six',
+       '7' => 'seven', '8' => 'eight', '9' => 'nine',
+       '10' => 'ten', '11' => 'eleven', '12' => 'twelve',
+       '13' => 'thirteen', '14' => 'fourteen',
+       '15' => 'fifteen', '16' => 'sixteen', '17' => 'seventeen',
+       '18' => 'eighteen', '19' =>'nineteen', '20' => 'twenty',
+       '30' => 'thirty', '40' => 'forty', '50' => 'fifty',
+       '60' => 'sixty', '70' => 'seventy',
+       '80' => 'eighty', '90' => 'ninety');
+       $digits = array('', 'hundred', 'thousand', 'lakh', 'crore');
+       while ($i < $digits_1) {
+           $divider = ($i == 2) ? 10 : 100;
+           $number = floor($no % $divider);
+           $no = floor($no / $divider);
+           $i += ($divider == 10) ? 1 : 2;
+           if ($number) {
+           $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
+           $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
+           $str [] = ($number < 21) ? $words[$number] .
+               " " . $digits[$counter] . $plural . " " . $hundred
+               :
+               $words[floor($number / 10) * 10]
+               . " " . $words[$number % 10] . " "
+               . $digits[$counter] . $plural . " " . $hundred;
+           } else $str[] = null;
+       }
+       $str = array_reverse($str);
+       $result = implode('', $str);
+       $points = ($point) ?
+       "." . $words[$point / 10] . " " .
+               $words[$point = $point % 10] : '';
+       $result_json = array('currency' => $result . "rupees " . $points . " paise");
+       header('Cache-Control: no-cache, must-revalidate');
+       header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+       header('Content-type: application/json');
+       echo json_encode($result_json);
+   }
+?>
+
+// MD5 Checksum
+<?php
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+       $string = $_POST['string'];
+       echo md5($string);
+   }
+?>
+
+// Bar Code
+<?php
+    require '../vendor/autoload.php';
+    function qrcode_generator($data)
+    {
+        $return_response = array();
+        $barcode = new \Com\Tecnick\Barcode\Barcode();
+        // creating folder to save images
+        $targetPath = "images/";
+        //if the target path does not exist create one
+        if (! is_dir($targetPath)) {
+            mkdir($targetPath, 0777, true);
+        }
+        // instantiating barcode class
+        $barcode = new \Com\Tecnick\Barcode\Barcode();
+        // creating barcode for the given data
+        $bobj = $barcode->getBarcodeObj('QRCODE,H', $data, 150, 150, 'black', array(0, 0, 0, 0))->setBackgroundColor('#ffffff');
+        $imageData = $bobj->getPngData();
+        $timestamp = time();
+        $files2 = glob( $targetPath ."*" ); 
+        if( $files2 ) 
+        { 
+            $filecount = count($files2); 
+        } 
+        // saving the png data
+        file_put_contents($targetPath . (string)$filecount . '.png', $imageData);
+        $return_response['filename'] = (string)$filecount.'.png';
+        $return_response['filepath'] = $targetPath;
+        return $return_response;
+}
+?>
+
+// OTP - Numeric
+<?php
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+       $n = $_POST['n'];
+       $generator = "1357902468";
+       $result = "";
+       for ($i = 1; $i <= $n; $i++) {
+           $result .= substr($generator, (rand()%(strlen($generator))), 1);
+       }
+       $result_json = array('otp' => $result);
+       header('Cache-Control: no-cache, must-revalidate');
+       header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+       header('Content-type: application/json');
+       if (empty($n)) {
+           echo "Incorrect number";
+       } else {
+           echo json_encode($result_json);
+       }
+   }
+?>
+
+// ALPHANUMERIC
+<?php
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+       $n = $_POST['n'];
+       $generator = "1357902468abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       $result = "";
+       for ($i = 1; $i <= $n; $i++) {
+           $result .= substr($generator, (rand()%(strlen($generator))), 1);
+       }
+       $result_json = array('otp' => $result);
+       header('Cache-Control: no-cache, must-revalidate');
+       header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+       header('Content-type: application/json');
+       if (empty($n)) {
+           echo "Incorrect number";
+       } else {
+           echo json_encode($result_json);
+       }
+   }
+?>
+
+// ALPHABET
+<?php
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+       $n = $_POST['n'];
+       $generator = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       $result = "";
+       for ($i = 1; $i <= $n; $i++) {
+           $result .= substr($generator, (rand()%(strlen($generator))), 1);
+       }
+       $result_json = array('otp' => $result);
+       header('Cache-Control: no-cache, must-revalidate');
+       header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+       header('Content-type: application/json');
+       if (empty($n)) {
+           echo "Incorrect number";
+       } else {
+           echo json_encode($result_json);
+       }
+   }
+?>
+
+<?php
+    // required headers
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Credentials: true");
+    header('Content-Type: application/json');
+    include "phpqrcode/qrlib.php";
+    $str=isset($_GET['string']) ? $_GET['string'] : die();
+    $res=array();
+    $res_str="";
+    $n=rand(1,100);
+    $s="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRST";
+    for($i=0;$i<5;$i++)
+    {
+        $ind=rand(1,strlen($s)-1);
+        $res_str.=$s[$ind];
+    }
+    $res_str.='_'.(string)$n;
+    $res_str.='_QR.png';
+    QRcode::png($str, $res_str, "L", 4, 4);
+    echo json_encode($res_str);
+?>
+
+// CAPTCHA
+<?php
+	session_start();
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$data = $_POST['data'];
+		$captcha = $data;
+		$_SESSION["captcha"] = $captcha;
+		$im = imagecreatetruecolor(50, 24);
+		$bg = imagecolorallocate($im, 22, 86, 165);
+		$fg = imagecolorallocate($im, 255, 255, 255);
+		imagefill($im, 0, 0, $bg);
+		imagestring($im, rand(1, 7), rand(1, 7), rand(1, 7), $captcha, $fg);
+		header("Cache-Control: no-store, no-cache, must-revalidate"); 
+		define('BASE_DIR', dirname(__FILE__).'/images/');
+		$file = BASE_DIR . 'images' . '.png';
+		imagepng($im,$file);
+		imagedestroy($im);
+		echo "Captcha saved";
+	}
+?>
